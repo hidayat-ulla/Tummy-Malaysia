1f093e4ddff79226a9359579cbc0404a842d2209
hidayat ullah <hu3014613@gmail.com>
2026-01-18 22:42:51 +0500

    your message

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV.VITE_BASE_URL || "/Tummy-Tuck-Malaysia",
+  base: process.env.VITE_BASE_PATH || "/Tummy-Tuck-Malaysia",
 })
